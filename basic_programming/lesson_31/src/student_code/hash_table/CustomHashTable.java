ef82d41929bbd9442ea930f4a96ead62f7fe7179
User <uzvarykoleksandr@gmail.com>
2024-01-29 21:48:48 +0200

    Merge remote-tracking branch 'origin/main'

--- basic_programming/lesson_31/src/student_code/hash_table/CustomHashTable.java
+++ basic_programming/lesson_31/src/student_code/hash_table/CustomHashTable.java
@@ -4,9 +4,65 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class CustomHashTable {
-    private ArrayList <LinkedList<Movie>> tale = new ArrayList<>();
+    private static int countOfBucket = 16;
+    private ArrayList <LinkedList<Movie>> table = new ArrayList<>();
 
     public CustomHashTable() {
 
     }
+
+
+
+    public boolean add (Movie movie){
+        if (contains(movie)){return false;}
+
+        int indexOfBucket = movie.hashCode()%countOfBucket;
+        LinkedList<Movie> listFromBucket = table.get(indexOfBucket);
+        if (listFromBucket == null){
+            listFromBucket = new LinkedList<Movie>();
+        }
+        listFromBucket.add(movie);
+
+        return true;
+    }
+
+   /* public ArrayList<Movie> toArray(){
+        ArrayList<Movie> movie = new ArrayList<>();
+        LinkedList<Movie> movies
+    }*/
+
+
+
+
+
+
+
+
+   public boolean contains (Movie movie) {
+
+    int movieHash = movie.hashCode();
+    int indexOfBucket = movieHash % countOfBucket;
+    LinkedList<Movie> listFromBucket = table.get(indexOfBucket);
+       if (listFromBucket == null || listFromBucket.isEmpty()) {
+           return false;
+       }
+
+       for (int i = 0; i < listFromBucket.size(); i++) {
+           if (listFromBucket.get(i).hashCode() == movieHash) {
+              if(movie.equals(listFromBucket.get(i))){
+                   return true;
+               }
+           }
+       }
+
+
+    return false;
+
+   }
+
+
+
+
+
+
 }
