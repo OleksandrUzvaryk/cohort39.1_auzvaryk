f2739d3e570d827c1dac7d86a757199b1abedb8c
User <uzvarykoleksandr@gmail.com>
2024-02-01 17:05:11 +0200

    Merge remote-tracking branch 'origin/main'

--- basic_programming/lesson_32/src/student_sandbox/StudentClassWork.java
+++ basic_programming/lesson_32/src/student_sandbox/StudentClassWork.java
@@ -1,4 +1,15 @@
 package student_sandbox;
 
+import java.util.HashSet;
+
 public class StudentClassWork {
+
+    public static void main(String[] args) {
+
+
+        HashSet<String> map = new HashSet<>(31,32);
+        map.add("a,");
+
+
+    }
 }
