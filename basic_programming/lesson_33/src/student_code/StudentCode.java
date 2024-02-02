f8cc56aa7b096a6547cb028a77bc8d6a1beb468d
User <uzvarykoleksandr@gmail.com>
2024-02-02 19:55:17 +0200

    Merge remote-tracking branch 'origin/main'

--- basic_programming/lesson_33/src/student_code/StudentCode.java
+++ basic_programming/lesson_33/src/student_code/StudentCode.java
@@ -1,7 +1,42 @@
 package student_code;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class StudentCode {
     public static void main(String[] args) {
+
+        String path = "basic_programming/lesson_33/src/student_code/file.txt";
+        String path2 = "basic_programming/lesson_33/src/student_code/";
+        String unExist = " ";
+        // C:\Users\User\IdeaProjects\cohort39.1_auzvaryk\basic_programming\lesson_33\src\student_code\file.txt
+
+        File file = new File(path); //    доступ к методам
+        File dir = new File(path2);
+        File unEx = new File(unExist);
+        System.out.println("file exist is => " + file.exists()); // метод существует или нет , тру фолс
+
+        System.out.println("Абсолютный путь к файлу => " + file.getAbsolutePath()); // путь к файлу
+
+        System.out.println("Размер файла  => " + file.length()); // количество символов в пути
+
+        System.out.println("Указание на директорию или файл=> " + file.isFile());
+        System.out.println("Указание на директорию или файл=> " + file.isDirectory());
+
+        System.out.println("Указание на директорию или файл=> " + dir.isFile());
+        System.out.println("Указание на директорию или файл=> " + dir.isDirectory());
+
+        //System.out.println("Существует ли файл => " + unEx.createNewFile());
+
+
+        System.out.println("Удален ли файл => " + unEx.delete());
+
+        System.out.println("" + file.listFiles());
+        System.out.println("" + Arrays.toString(dir.listFiles()));
+
+
+
+
 
     }
 }
