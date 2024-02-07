8fd11260db818f45accd742cb0c6cde956bae51d
User <uzvarykoleksandr@gmail.com>
2024-02-07 18:06:15 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ basic_programming/lesson_34/src/Hometask/Task1.java
@@ -0,0 +1,16 @@
+package Hometask;
+
+import java.util.Stack;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        String[] inputs = {"( [ ] { } )", "[(])", "{[( )]}", "{[}"};
+        for (String input : inputs) {
+            System.out.println("Input: " + input + " Output: ");
+
+
+        }
+    }
+}
