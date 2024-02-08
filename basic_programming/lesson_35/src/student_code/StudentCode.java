3d3f4bf9a8d1507cfd2b52cb4b4bb51f3f6988d6
User <uzvarykoleksandr@gmail.com>
2024-02-08 18:15:47 +0200

    Merge remote-tracking branch 'origin/main'

--- basic_programming/lesson_35/src/student_code/StudentCode.java
+++ basic_programming/lesson_35/src/student_code/StudentCode.java
@@ -1,20 +1,23 @@
 package student_code;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 
 public class StudentCode {
     public static void main(String[] args) {
 
-        ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of("Europa/Paris"));
+       /* ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of("Europa/Paris"));
         ZonedDateTime zdt2 = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
 
         System.out.println(zdt);
         System.out.println(zdt2);
 
+*/
 
-
-
+        LocalDate ldt = LocalDate.of(2024,2,1);
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime localDateTime = LocalDateTime.of(ldt,LocalTime.of(0,0));
+        Duration dur = Duration.between(localDateTime,now);
+        System.out.println(dur);
 
 
 
