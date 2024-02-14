4553a10c6d9cabcc11a287d4b379566eb8b9fc23
User <uzvarykoleksandr@gmail.com>
2024-02-14 20:40:37 +0200

    Merge remote-tracking branch 'm39/main'

--- basic_programming/lesson_37_12_02_functional_interface/src/teacher_code/SecondFuncInterface.java
+++
@@ -1,6 +0,0 @@
-package teacher_code;
-
-@FunctionalInterface
-public interface SecondFuncInterface {
-    abstract void doSomething();
-}
--- basic_programming/lesson_38/src/student_code/StudentCode.java
+++ basic_programming/lesson_38/src/student_code/StudentCode.java
@@ -1,11 +1,7 @@
 package student_code;
 
-import java.util.Collection;
-import java.util.Set;
-
 public class StudentCode {
     public static void main(String[] args) {
-
 
     }
 }
