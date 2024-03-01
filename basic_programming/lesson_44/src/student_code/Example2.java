033e396918191e159cdad524dcb185a96d384b48
User <uzvarykoleksandr@gmail.com>
2024-03-01 11:16:35 +0200

    Merge remote-tracking branch 'm39/main'

--- basic_programming/lesson_44/src/student_code/Example2.java
+++ basic_programming/lesson_44/src/student_code/Example2.java
@@ -3,7 +3,7 @@ package student_code;
 public class Example2 {
 
     public static void main(String[] args) {
-       int x = 120;
+       int x = 1_000_000_009;
        reverse(x);
         System.out.println(reverse(x));
 
@@ -21,6 +21,7 @@ public class Example2 {
         // отрезаем последнюю цифру
     int lastNumber = number % 10; // 123/10-> 3, 12/10 -> 2, 1/10 ->1
         // число которое возвращаем - набираем с конца
+
     reverseNumber = reverseNumber * 10 + lastNumber; // 0 + 3 =3, 3*10 + 2= 32, 32*10 + 1= 321
         // уменьшаем условие цикла
     number = number/10; // 123, 12,1
